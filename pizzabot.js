424ed3b08c55578a6534e58d49466beb599e87e2
Paul Wyatt <Paul.Wyatt@brightblueday.com>
2016-12-01 08:49:31 +0000

    session

--- pizzabot.js
+++ pizzabot.js
@@ -1,14 +1,37 @@
-module.exports = function (req, res, next) {
-	var userName = req.body.user_name;
-	
-	var botPayload = {
-		text : 'Hello, ' + userName + '!'
-	};
+var apiai = require('apiai');
 
-	// avoid infinite loop
-	if (userName !== 'slackbot') {
-		return res.status(200).json(botPayload);
-	} else {
-		return res.status(200).end();
+var accessToken = "6a0a368df7ee4efa8ffa48a8d73c64bb";
+var baseUrl = "https://api.api.ai/v1/";
+
+module.exports = function (req, res, next) {
+	var app = apiai(accessToken);
+	var question = req.body.text;
+	
+	console.log(req.body.token);
+	
+	if(req.body.token != undefined){
+		var slackUserToken = req.body.token
+		var request = app.textRequest(question, {
+			sessionId: slackUserToken
+		});
+	}else{
+		var request = app.textRequest(question, {
+			sessionId: 'somerandomthing'
+		});
 	}
+	
+	request.on('response', function(response) {
+		var result = {
+			text : response.result.fulfillment.speech
+		}
+		
+		return res.status(200).json(result);
+	});
+ 
+	request.on('error', function(error) {
+		console.log(error);
+		return res.status(200).end();
+	});
+ 
+	request.end();
 }
