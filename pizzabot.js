d1b821acb158fbe93af308ea43a91a0d0229666f
Paul Wyatt <Paul.Wyatt@brightblueday.com>
2016-12-01 10:14:46 +0000

    express folders

--- pizzabot.js
+++ pizzabot.js
@@ -1,15 +1,11 @@
-var apiai = require('apiai');
-
-var accessToken = "6a0a368df7ee4efa8ffa48a8d73c64bb";
-var baseUrl = "https://api.api.ai/v1/";
-
 module.exports = function (req, res, next) {
+	var apiai = require('apiai');
+	var accessToken = "<need key>";
+	var baseUrl = "https://api.api.ai/v1/";
 	var app = apiai(accessToken);
 	var question = req.body.text;
 	
 	question = question.toLowerCase().replace("pizzabot", "").trim();
-	
-	console.log(req.body.token);
 	
 	if(req.body.token != undefined){
 		var slackUserToken = req.body.token
