2a91eea32161369805555bdc92d6431ff7701036
Paul Wyatt <Paul.Wyatt@brightblueday.com>
2016-12-01 10:05:37 +0000

    web

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var hellobot = require('./pizzabot');
+var path = require("path");
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -9,13 +10,15 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // test route
-app.get('/', function (req, res) { res.status(200).send('Hello world!') });
+app.get('/test', function (req, res) { res.status(200).send('Hello world!') });
 
 // main routes
+app.get('/',function(req,res){       
+	//__dirname : It will resolve to your project folder.
+	res.sendFile(path.join(__dirname+'/chattypizza.html'));
+});
+
 app.post('/hello', hellobot);
-
-
-
 
 // error handler
 app.use(function (err, req, res, next) {
